34e3043e8ce762eb0b24299605b502abf0a02981
x-torix <martyna@piwowarska.com.pl>
2022-11-16 15:16:04 +0100

    last school commit

--- class.php
+++ class.php
@@ -1,5 +1,5 @@
 <?php
-class User{
+class User {
     private int $id;
     private string $login;
     private string $password;
@@ -9,6 +9,26 @@ class User{
         public function __construct(string $login, string $password) {
             $this->login = $login;
             $this->password = $password;
+            $this->firstname = "";
+            $this->lastname = "";
+        }
+        
+        public function register() {
+            $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+            $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+            $db = new mysqli('localhost', 'root', '', 'loginform');
+            $preparedQuery = $db->prepare($query);
+            $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstname, $this->lastname);
+            $preparedQuery->execute();
+        }
+
+        public function login() {
+            $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+            $db = new mysqli ('localhost', 'root', '', 'loginform');
+            $preparedQuery = $db->prepare($query);
+            $preparedQuery->bind_param('s', $this->login); 
+            $preparedQuery->execute();
+            
         }
 
 }
