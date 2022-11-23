8dd599f7a9141a74a0444b57105f16c0973edf5b
x-torix <martyna@piwowarska.com.pl>
2022-11-23 14:56:07 +0100

    correct register 2

--- register.php
+++ register.php
@@ -1,6 +1,13 @@
 <?php
 require_once('config.php');
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+    if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
+    || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+        //podano pusty string jako jedną z wymaganych wartości
+        $twig->display('message.html.twig', 
+                        ['message' => "Nie podano wymaganej wartości"]);
+        exit();
+    }
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     $user->setFirstName($_REQUEST['firstName']);
     $user->setLastName($_REQUEST['lastName']);
