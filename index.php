11953a99c70c7340ebc692d986173fd0e871f20f
x-torix <martyna@piwowarska.com.pl>
2022-11-30 13:20:12 +0100

    13:20 30.11

--- index.php
+++ index.php
@@ -10,16 +10,52 @@ Route::add('/', function(){
 });
 
 Route::add('/login', function(){
-    //echo "strona logowania";
     global $twig;
     $twig->display('login.html.twig');
 });
 
-Route::add('/register', function(){
-    //echo "strona rejestracji";
+Route::add('/login', function(){
     global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            $twig->display('login.html.twig', ['message'=> "Błędny login lub hasło"]);
+        }
+    } else {
+        die("nie otrzymano danych");
+    }
+    $twig->display('login.html.twig');
+},'post');
+
+Route::add('/register', function(){
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
+        || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+            $twig->display('register.html.twig', 
+                            ['message' => "Nie podano wymaganej wartości"]);
+            exit();
+        }
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        $user->setFirstName($_REQUEST['firstName']);
+        $user->setLastName($_REQUEST['lastName']);
+        if($user->register()) {
+            $twig->display('message.html.twig', 
+                                ['message' => "Zarejestrowano poprawnie"]);
+        } else {
+            $twig->display('register.html.twig', 
+                                ['message' => "Błąd rejestracji użytkownika"]);
+        }
+    }else {
+        die("nie otrzymano danych");
+    }
     $twig->display('register.html.twig');
-});
+},'post');
 
 
 
